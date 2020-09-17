67573171f2046951726d17ab2558db3302e30ecb
Michael Bogner <michael.bogner@mesacc.edu>
2020-09-17 09:54:26 -0700

    update viewController message to Solar System

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        say("Hello Universe")
+        say("Hello Solar System")
         
     }
     
